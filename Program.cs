0cd7d6cc1fa1e879d383451a757a5447a162366c
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2021-03-31 17:20:28 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LeanCode.ContractsGeneratorV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
