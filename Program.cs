a487f89db33dc8598652945c320bd2eb7ae1d450
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-04-06 16:15:18 +0200

    Save both JSON & Protobuf example file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Text;
+using Google.Protobuf;
 
 namespace LeanCode.ContractsGeneratorV2
 {
@@ -6,13 +8,32 @@ namespace LeanCode.ContractsGeneratorV2
     {
         static void Main(string[] args)
         {
-            var contracts = new ContractsCompiler("./ExampleContracts").Compile();
-            var gen = new ContractsGenerator(contracts);
-            var generated = gen.Generate("./ExampleContracts");
+            var fullPath = Path.GetFullPath(args.Length > 1 ? args[1] : "./ExampleContracts");
+            Write(fullPath);
+            var export = Read();
+            WriteJson(export);
+        }
 
-            using var outputStream = File.OpenWrite("./example.contracts");
-            using var codedOutput = new Google.Protobuf.CodedOutputStream(outputStream, true);
+        private static void Write(string path)
+        {
+            var contracts = new ContractsCompiler(path).Compile();
+            var gen = new ContractsGenerator(contracts);
+            var generated = gen.Generate(path);
+            using var outputStream = File.OpenWrite("./example.pb");
+            using var codedOutput = new CodedOutputStream(outputStream, true);
             generated.WriteTo(codedOutput);
+        }
+
+        private static Export Read()
+        {
+            using var inputStream = File.OpenRead("./example.pb");
+            return Export.Parser.ParseFrom(inputStream);
+        }
+
+        private static void WriteJson(Export export)
+        {
+            using var writer = File.CreateText("./example.json");
+            writer.Write(export.ToString());
         }
     }
 }
---
+++ example.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6163d473ed0b3532e5aa12afca364829c67fa543988635cdcee48c2f69f20525
+size 2273
