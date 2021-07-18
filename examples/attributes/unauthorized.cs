3b2d5bb85b6b56bf8be61db7e7bc4e4d6ac238c3
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-07-18 13:15:36 +0200

    Add tests for attributes

---
+++ examples/attributes/unauthorized.cs
@@ -0,0 +1,5 @@
+using LeanCode.CQRS;
+using LeanCode.CQRS.Security;
+
+[AllowUnauthorized]
+public class A : IRemoteCommand { }
---
+++ src/LeanCode.ContractsGenerator.Tests/AttributeArgumentExtensions.cs
@@ -0,0 +1,29 @@
+namespace LeanCode.ContractsGenerator.Tests
+{
+    public static class AttributeArgumentExtensions
+    {
+        public static AttributeArgument Named(string name, object? value)
+        {
+            return new()
+            {
+                Named = new()
+                {
+                    Name = name,
+                    Value = value.ToValueRef(),
+                },
+            };
+        }
+
+        public static AttributeArgument Positional(int pos, object? value)
+        {
+            return new()
+            {
+                Positional = new()
+                {
+                    Position = pos,
+                    Value = value.ToValueRef(),
+                },
+            };
+        }
+    }
+}
