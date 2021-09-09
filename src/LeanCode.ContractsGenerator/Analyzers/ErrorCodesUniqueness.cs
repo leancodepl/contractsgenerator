86f8208742c648e9ad80d0a190206b0f67581611
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-09-09 18:55:24 +0200

    Add error codes uniqueness analyzer

---
+++ src/LeanCode.ContractsGenerator/Analyzers/ErrorCodesUniqueness.cs
@@ -0,0 +1,54 @@
+namespace LeanCode.ContractsGenerator.Analyzers
+{
+    public class ErrorCodesUniqueness : BaseAnalyzer
+    {
+        public const string Code = "LNCD003";
+
+        public override IEnumerable<AnalyzeError> AnalyzeErrorCodes(IEnumerable<ErrorCode> errorCodes)
+        {
+            return errorCodes
+                .SelectMany(Flatten)
+                .GroupBy(e => e.Code)
+                .Where(g => g.Count() > 1)
+                .Select(g => new AnalyzeError(Code, $"Duplicate error codes: {string.Join(", ", g.Select(c => c.Name))}", nameof(ErrorCode), ""));
+
+            static IEnumerable<ErrorCode.Types.Single> Flatten(ErrorCode errCode)
+            {
+                if (errCode.Single is ErrorCode.Types.Single s)
+                {
+                    return new[] { s };
+                }
+                else
+                {
+                    return errCode.Group.InnerCodes.SelectMany(Flatten);
+                }
+            }
+        }
+
+        public override IEnumerable<AnalyzeError> Analyze(Export export)
+        {
+            return export.Statements
+                .SelectMany(AnalyzeStatement)
+                .ToList();
+        }
+
+        public override IEnumerable<AnalyzeError> AnalyzeCommand(Statement stmt, Statement.Types.Command command)
+        {
+            return command.ErrorCodes
+                .SelectMany(AnalyzeErrorCode)
+                .Select(e => e with { Name = stmt.Name });
+        }
+
+        public override IEnumerable<AnalyzeError> AnalyzeStatement(Statement stmt)
+        {
+            if (stmt.Command is Statement.Types.Command cmd)
+            {
+                return AnalyzeCommand(stmt, cmd);
+            }
+            else
+            {
+                return Enumerable.Empty<AnalyzeError>();
+            }
+        }
+    }
+}
