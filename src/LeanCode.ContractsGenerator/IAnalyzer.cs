3aad1031e9e1abdf405f55e190f2cfd19c7a9449
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-09-09 18:53:51 +0200

    Add base analyzer

---
+++ src/LeanCode.ContractsGenerator/IAnalyzer.cs
@@ -0,0 +1,9 @@
+namespace LeanCode.ContractsGenerator
+{
+    public interface IAnalyzer
+    {
+        IEnumerable<AnalyzeError> Analyze(Export export);
+    }
+
+    public record AnalyzeError(string Code, string Message, string Type, string Name);
+}
