4fba7da3657e0851518ef044592f381898b73bc5
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-10-28 22:57:36 +0200

    Test more context tracking cases

--- src/LeanCode.ContractsGenerator.Tests/ExampleBased/Analyzers/Context.cs
+++ src/LeanCode.ContractsGenerator.Tests/ExampleBased/Analyzers/Context.cs
@@ -12,6 +12,9 @@ public class Context
                 .WithError("CNTR0004", "Dto1.A<0: System.Decimal>")
                 .WithError("CNTR0004", "Dto2.A<0: System.Decimal>")
                 .WithError("CNTR0004", "Dto3:System.IDisposable")
+                .WithError("CNTR0004", "Dto4:System.IDisposable")
+                .WithError("CNTR0004", "Dto5:Inner<0: System.Decimal>")
+                .WithError("CNTR0004", "Dto6:Inner<0: Inner><0: System.Decimal>")
                 .WithError("CNTR0004", "Query1->System.Decimal")
                 .WithError("CNTR0004", "Query2->Inner<0: System.Decimal>");
     }
