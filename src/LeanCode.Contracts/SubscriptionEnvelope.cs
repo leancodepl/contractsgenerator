b0f2f05b49e1eb871a37d4bc79a926c4b24483a6
Wojciech Klusek <wojciech.klusek@leancode.pl>
2023-07-25 11:54:27 +0200

    Use JsonDocument instead of string in SubscriptionEnvelope

--- src/LeanCode.Contracts/SubscriptionEnvelope.cs
+++ src/LeanCode.Contracts/SubscriptionEnvelope.cs
@@ -1,8 +1,10 @@
+using System.Text.Json;
+
 namespace LeanCode.Contracts;
 
 public sealed class SubscriptionEnvelope
 {
     public Guid Id { get; set; }
     public string TopicType { get; set; } = default!;
-    public string Topic { get; set; } = default!;
+    public JsonDocument Topic { get; set; } = default!;
 }
