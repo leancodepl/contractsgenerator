f2589b5756780aff76a204d8bcbd1c01feea4463
Łukasz Komoszyński <lukasz.komoszynski@leancode.pl>
2023-11-08 12:20:34 +0100

    Add LeanCode.Contracts.Admin

---
+++ src/LeanCode.Contracts.Admin/AdminQuery.cs
@@ -0,0 +1,52 @@
+﻿namespace LeanCode.Contracts.Admin;
+
+public abstract class AdminQuery<TResult> : IQuery<AdminQueryResult<TResult>>
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+
+    public SortOrderDTO? SortOrder { get; set; }
+    public string? SortBy { get; set; }
+}
+
+public enum SortOrderDTO
+{
+    Descending = 0,
+    Ascending = 1
+}
+
+public class AdminQueryResult<TResult>
+{
+    public long Total { get; set; }
+    public List<TResult> Items { get; set; }
+}
+
+public class AdminFilterRange<T>
+{
+    public T? From { get; set; }
+    public T? To { get; set; }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class AdminFilterFor : Attribute
+{
+    public AdminFilterFor(string name) { }
+}
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public class AdminLabel : Attribute
+{
+    public AdminLabel(string label) { }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class AdminColumn : Attribute
+{
+    public AdminColumn(string? name) { }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class AdminSortable : Attribute
+{
+    public AdminSortable() { }
+}
