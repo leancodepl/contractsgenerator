22ec14811674508c76311b3c7b47f487ca07abc5
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2026-07-10 16:03:24 +0200

    Fix ABI break in ValidationError's constructors

--- src/LeanCode.Contracts/Validation/ValidationError.cs
+++ src/LeanCode.Contracts/Validation/ValidationError.cs
@@ -1,7 +1,11 @@
 namespace LeanCode.Contracts.Validation;
 
-public class ValidationError(string propertyName, string errorMessage, int errorCode, string? errorName = null)
+[method: System.Text.Json.Serialization.JsonConstructor]
+public class ValidationError(string propertyName, string errorMessage, int errorCode, string? errorName)
 {
+    public ValidationError(string propertyName, string errorMessage, int errorCode)
+        : this(propertyName, errorMessage, errorCode, null) { }
+
     public string PropertyName { get; } = propertyName;
     public string ErrorMessage { get; } = errorMessage;
     public int ErrorCode { get; } = errorCode;
